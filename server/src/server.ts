c6b8e3c8a3c2369cb3add7ea555616bc0de7b331
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-01 11:41:01 -0300

    Estrutura inicial do Back-End

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('Listagem de usuários');
+
+  response.json([
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Raphael'
+  ]);
+})
+
+app.listen(3333);
