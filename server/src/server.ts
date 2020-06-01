2b7f13f6879dbadea02b70029ba636d88df58eb5
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-01 12:16:17 -0300

    Adicionado padronização de código com ESlint + Prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,12 +5,7 @@ const app = express();
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
 
-  response.json([
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Raphael'
-  ]);
-})
+  response.json(['Diego', 'Cleiton', 'Robson', 'Raphael']);
+});
 
 app.listen(3333);
