8e370b2fd733da6db20506570c284bde70a73a5e
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-02 21:18:04 -0300

    feat: :sparkles: finalizado Back-end

---
+++ server/src/database/migrations/02_create_points_items.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', table => {
+    table.increments('id').primary();
+
+    table.integer('point_id').notNullable().references('id').inTable('points');
+
+    table.string('item_id').notNullable().references('id').inTable('items');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  response.json(['Diego', 'Cleiton', 'Robson', 'Raphael']);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
