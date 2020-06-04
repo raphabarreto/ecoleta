4debeac3bf3a61eadab36400fd7a6ce974e05f37
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-04 16:24:03 -0300

    feat: adicionado padronização de código com ESlint + Prettier

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function App() {
   return (
+    // eslint-disable-next-line
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
